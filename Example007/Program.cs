738727b9569b2ca2250486c2ce3d1f4b20019937
Zimakova Yulia <Zimakova86@yandex.ru>
2023-07-02 23:16:08 +0300

    oh

---
+++ Example007/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+//Console.SetCursorPosition(10, 4);
+//Console.WriteLine("+");
+
+int xa = 15, ya = 5, xb = 1, yb = 8, xc = 7, yc = 15;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yb);
+Console.WriteLine("+");
