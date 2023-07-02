4ba890a2a250f13bb30e5917e4b2a92e31ef4b49
Zimakova Yulia <Zimakova86@yandex.ru>
2023-07-02 18:19:13 +0300

    hello masha added

---
+++ Example006/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Write yout name");
+string username = Console.ReadLine();
+
+if(username.ToLower() == " маша")
+{
+    Console.WriteLine("OMG! It's Masha! Hello, my dear!");
+}
+else
+{
+    Console.Write("Hello");
+    Console.WriteLine(username);
+}
