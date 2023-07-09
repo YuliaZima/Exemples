43d1a9312a8ec5e25dabcc03bd7b269c6fca7140
Zimakova Yulia <Zimakova86@yandex.ru>
2023-07-09 23:58:16 +0300

    format added

--- Csharp/Program.cs
+++ Csharp/Program.cs
@@ -1,79 +1,78 @@
 ﻿void Task10()
 {
-        Random rnd = new Random();
-        int number = rnd.Next(100,1000);
-        Console.WriteLine($"Выпало случайное число: {number}");
+    Random rnd = new Random();
+    int number = rnd.Next(100, 1000);
+    Console.WriteLine($"Выпало случайное число: {number}");
 
-    
-        int f_digit = number/10;
-        int s_digit = f_digit%10;
-        
 
-        Console.WriteLine(s_digit);
+    int f_digit = number / 10;
+    int s_digit = f_digit % 10;
+
+
+    Console.WriteLine(s_digit);
 }
 
 void Task15()
 {
-       /* Random rnd = new Random();
-        int number = rnd.Next(1,8);
-        Console.WriteLine($"Выпало случайное число: {number}");
+    /* Random rnd = new Random();
+     int number = rnd.Next(1,8);
+     Console.WriteLine($"Выпало случайное число: {number}");
 
-    if (number < 6)
-        {
-            Console.WriteLine($"{number} - не является выходным");
-        }
+ if (number < 6)
+     {
+         Console.WriteLine($"{number} - не является выходным");
+     }
 
-    else 
-        {
-            Console.WriteLine($"{number} - является выходным");
-        }*/
-
-
-        
+ else 
+     {
+         Console.WriteLine($"{number} - является выходным");
+     }*/
 
 }
-    void Sample(int number)
+
+void Sample(int number)
+{
+    if (number > 1 && number <= 5)
     {
-        if (number > 1 && number <= 5)
-        {
-            System.Console.WriteLine("День не является выходным");
-        } 
-        else if(number>5 && number <=7)
-        {
-            System.Console.WriteLine("День является выходным");
-        }
-        else
-        {
-            System.Console.WriteLine("Такого дня недели нет");
-        }
+        System.Console.WriteLine("День не является выходным");
+    }
+    else if (number > 5 && number <= 7)
+    {
+        System.Console.WriteLine("День является выходным");
+    }
+    else
+    {
+        System.Console.WriteLine("Такого дня недели нет");
+    }
 }
 
 
 void Task13()
 {
-        Random rnd = new Random();
-        int number = rnd.Next();
-        Console.WriteLine($"Выпало случайное число: {number}");
+    Random rnd = new Random();
+    int number = rnd.Next();
+    Console.WriteLine($"Выпало случайное число: {number}");
 
 
-        if (number<100)
+    if (number < 100)
+    {
+        Console.WriteLine("третьей цифры нет");
+    }
+    else
+    {
+        while (number >= 1000)
         {
-            Console.WriteLine("третьей цифры нет");
+            number = number / 10;
+
         }
-        else
-        {
-            while (number >= 1000)
-            {
-                 number = number/10;
-                 
-            }
-            Console.WriteLine(number%10);
-            
-        }
-        
-            
-        
+        Console.WriteLine(number % 10);
+
+    }
 
 }
 
+Task10();
+Task15();
+Sample(6);
+Task13();
 
